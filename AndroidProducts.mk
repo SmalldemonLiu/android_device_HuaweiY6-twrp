b6a6f8f4b480a2f32b5e3e2a2e95bf62c40a1765
SmalldemonLiu <47385468+SmalldemonLiu@users.noreply.github.com>
2022-11-26 17:34:06 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,11 +1,15 @@
 #
-# Copyright (C) 2012 The CyanogenMod Project
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#
+# Copyright (C) 2019-2022 The OrangeFox Recovery Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,5 +19,6 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_scl.mk \
-    $(LOCAL_DIR)/omni_scl.mk
+     $(LOCAL_DIR)/twrp_scl.mk
+
+COMMON_LUNCH_CHOICES := twrp_scl-eng
