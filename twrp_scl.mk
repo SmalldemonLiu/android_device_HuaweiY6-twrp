8696c1f76d29fb99c6a7ba6ea9f25d4f0d7c4673
SmalldemonLiu <47385468+SmalldemonLiu@users.noreply.github.com>
2022-11-28 19:29:46 +0800

    Update twrp_scl.mk

--- twrp_scl.mk
+++ twrp_scl.mk
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
